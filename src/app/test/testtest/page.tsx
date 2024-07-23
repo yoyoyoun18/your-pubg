9c0f63a7bbaeb52856f20381d32f2c000e12bfb0
kimyoungjo <yoyounn88@gmail.com>
2024-07-23 09:40:54 +0900

    test: 파일 시스템 라우팅 테스트 test와 testtest 폴더를 생성하여 파일 시스템 라우팅 파악

---
+++ src/app/test/testtest/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TestPage: React.FC = () => {
+  return (
+    <div>
+      <h1>TestTest Page</h1>
+      <p>This is the /test/testtest page.</p>
+    </div>
+  );
+};
+
+export default TestPage;
