4c95b0626cbd5eb783243173cd06caac0169e459
kimyoungjo <yoyounn88@gmail.com>
2024-07-31 19:47:43 +0900

    style: PlayerRank component 디자인 변경

--- src/components/PlayerRank.tsx
+++ src/components/PlayerRank.tsx
@@ -32,8 +32,6 @@ const PlayerRank = () => {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
-  console.log(data?.match.rankings[0]);
-
   return (
     <div className="bg-white flex items-center justify-center text-xs">
       <ul className="divide-y divide-gray-200 w-full h-full">
@@ -45,11 +43,58 @@ const PlayerRank = () => {
             <div className="col-span-2 flex items-center text-start">
               {index + 1}
             </div>
-            <div className="col-span-8 flex items-center text-start">
-              <span className="text-blue-500 cursor-pointer">
-                {player.name}
-              </span>
-            </div>
+            {index === 0 && (
+              <div className=" col-span-8 flex items-center justify-between text-start">
+                <span className="text-blue-500 text-sm flex items-start cursor-pointer">
+                  {player.name}
+                </span>
+                <span className="relative h-24 w-20 overflow-hidden">
+                  <img
+                    src="https://nng-phinf.pstatic.net/MjAyNDAxMTdfMjkw/MDAxNzA1NDcwNjIzODMx.A-2YsVv7nDynxRnLsHP21BKkOjhgIooRAw2goyplMKgg.LN11mrRnf1nPvOsDG237hsHkKlFflC0OsUKQDmHGs30g.PNG/KYEGfRPMGJktLXdrWeXS.png"
+                    alt="Player"
+                    className="absolute inset-0 object-cover w-full h-full"
+                  />
+                  <span className=" absolute inset-0 bg-gradient-to-t from-white to-transparent "></span>
+                </span>
+              </div>
+            )}
+            {index === 1 && (
+              <div className=" col-span-8 flex items-center justify-between text-start">
+                <span className="relative h-24 w-20 overflow-hidden">
+                  <img
+                    src="https://nng-phinf.pstatic.net/MjAyNDAxMTdfMjkw/MDAxNzA1NDcwNjIzODMx.A-2YsVv7nDynxRnLsHP21BKkOjhgIooRAw2goyplMKgg.LN11mrRnf1nPvOsDG237hsHkKlFflC0OsUKQDmHGs30g.PNG/KYEGfRPMGJktLXdrWeXS.png"
+                    alt="Player"
+                    className="absolute inset-0 object-cover w-full h-full"
+                  />
+                  <span className="absolute inset-0 bg-gradient-to-t from-white to-transparent "></span>
+                </span>
+                <span className="text-blue-500 text-sm flex items-start cursor-pointer">
+                  {player.name}
+                </span>
+              </div>
+            )}
+            {index === 2 && (
+              <div className=" col-span-8 flex items-center justify-between text-start">
+                <span className="text-blue-500 text-sm flex items-start cursor-pointer">
+                  {player.name}
+                </span>
+                <span className="relative h-24 w-20 overflow-hidden">
+                  <img
+                    src="https://nng-phinf.pstatic.net/MjAyNDAxMTdfMjkw/MDAxNzA1NDcwNjIzODMx.A-2YsVv7nDynxRnLsHP21BKkOjhgIooRAw2goyplMKgg.LN11mrRnf1nPvOsDG237hsHkKlFflC0OsUKQDmHGs30g.PNG/KYEGfRPMGJktLXdrWeXS.png"
+                    alt="Player"
+                    className="absolute inset-0 object-cover w-full h-full"
+                  />
+                  <span className="absolute inset-0 bg-gradient-to-t from-white to-transparent "></span>
+                </span>
+              </div>
+            )}
+            {index > 2 && (
+              <div className="col-span-8 flex items-center justify-between text-start">
+                <span className="text-blue-500 cursor-pointer">
+                  {player.name}
+                </span>
+              </div>
+            )}
             <div className="col-span-2 text-center">{player.point}</div>
           </li>
         ))}
