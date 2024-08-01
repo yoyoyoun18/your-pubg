b233ef4da6042fe82b29e5c712cdf3bbb353c854
kimyoungjo <yoyounn88@gmail.com>
2024-08-01 10:44:55 +0900

    feat: team rank에 team logo 구현

--- src/components/TeamRank.tsx
+++ src/components/TeamRank.tsx
@@ -9,9 +9,19 @@ interface TeamRanking {
   name: string;
 }
 
+interface TeamLogo {
+  url: string;
+}
+
 interface LeaderboardData {
   match: {
     rankings: TeamRanking[];
+  };
+}
+
+interface TeamLogoData {
+  [teamName: string]: {
+    url: string;
   };
 }
 
@@ -20,31 +30,58 @@ const fetchLeaderboard = async (): Promise<LeaderboardData> => {
   return response.data;
 };
 
+const fetchTeamLogo = async (): Promise<TeamLogoData> => {
+  const response = await axios.get("http://localhost:3000/api/team-logos");
+  return response.data;
+};
+
 const TeamRank = () => {
-  const { data, isLoading, error } = useQuery<LeaderboardData, Error>({
+  const {
+    data: leaderboardData,
+    isLoading: leaderboardIsLoading,
+    error: leaderboardError,
+  } = useQuery<LeaderboardData, Error>({
     queryKey: ["teamrank"],
     queryFn: fetchLeaderboard,
     staleTime: 60000,
     gcTime: 3600000,
   });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  const {
+    data: logoData,
+    isLoading: logoIsLoading,
+    error: logoError,
+  } = useQuery<TeamLogoData, Error>({
+    queryKey: ["teamlogo"],
+    queryFn: fetchTeamLogo,
+    staleTime: 60000,
+    gcTime: 3600000,
+  });
 
-  console.log(data?.match.rankings[0]);
+  if (leaderboardIsLoading || logoIsLoading) return <div>Loading...</div>;
+  if (leaderboardError) return <div>Error: {leaderboardError.message}</div>;
+  if (logoError) return <div>Error: {logoError.message}</div>;
 
   return (
     <div className="bg-white flex items-center justify-center text-xs">
       <ul className="divide-y divide-gray-200 w-full h-full">
-        {data?.match.rankings.map((team, index) => (
-          <li key={index} className="grid grid-cols-12 gap-4 p-3 items-center ">
+        {leaderboardData?.match.rankings.map((team, index) => (
+          <li key={index} className="grid grid-cols-12 gap-4 p-3 items-center">
             <div className="col-span-2 flex items-center text-start">
               {index + 1}
+            </div>
+            <div className="col-span-2 flex items-center">
+              {logoData && logoData[team.name] && (
+                <img
+                  src={logoData[team.name].url}
+                  alt={`${team.name} logo`}
+                  className="w-6 h-6 object-contain"
+                />
+              )}
             </div>
             <div className="col-span-8 flex items-center text-start">
               <span className="text-blue-500 cursor-pointer">{team.name}</span>
             </div>
-            {/* <div className="col-span-2 text-center">{team.point}</div> */}
           </li>
         ))}
       </ul>
