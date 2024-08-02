182211245163b6d4d678b7192bef28ee93c2a5b1
kimyoungjo <yoyounn88@gmail.com>
2024-08-02 15:20:59 +0900

    init: zuntand 전역 상태 설정 전적 검색에 사용할 user 정보를 저장하는 전역 상태를 설정

---
+++ src/store/useUserStore.ts
@@ -0,0 +1,45 @@
+// /src/store/useUserStore.ts
+import create from "zustand";
+
+interface TargetUser {
+  puuid: string;
+  gameName: string;
+  tagLine: string;
+  encryptedId: string;
+  accountId: string;
+  profileIconId: number;
+  revisionDate: number;
+  summonerLevel: number;
+  tier: string;
+  leaguePoints: number;
+  wins: number;
+  loses: number;
+}
+
+interface UserStoreState {
+  targetUser: TargetUser;
+  setTargetUser: (newUser: Partial<TargetUser>) => void;
+}
+
+const useUserStore = create<UserStoreState>((set) => ({
+  targetUser: {
+    puuid: "",
+    gameName: "",
+    tagLine: "",
+    encryptedId: "",
+    accountId: "",
+    profileIconId: 0,
+    revisionDate: 0,
+    summonerLevel: 0,
+    tier: "",
+    leaguePoints: 0,
+    wins: 0,
+    loses: 0,
+  },
+  setTargetUser: (newUser) =>
+    set((state) => ({
+      targetUser: { ...state.targetUser, ...newUser },
+    })),
+}));
+
+export default useUserStore;
