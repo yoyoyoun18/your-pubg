b02e62fb0dcd05c3b115ec349661218b78e9f92c
kimyoungjo <yoyounn88@gmail.com>
2024-08-02 15:22:19 +0900

    init: chart 관련 컴포넌트 생성

---
+++ src/components/StatChart.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
+
+function StatChart() {
+  const data = {
+    labels: ["Win", "Lose"],
+    datasets: [
+      {
+        data: [49, 51],
+        backgroundColor: ["#36A2EB", "#FF6384"],
+        hoverBackgroundColor: ["#36A2EB", "#FF6384"],
+      },
+    ],
+  };
+
+  const options: any = {
+    plugins: {
+      datalabels: {
+        color: "#fff",
+        formatter: (value: any) => {
+          return value + "%";
+        },
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context: any) {
+            let label = context.label || "";
+            if (label) {
+              label += ": ";
+            }
+            if (context.raw !== null) {
+              label += context.raw + "%";
+            }
+            return label;
+          },
+        },
+      },
+      legend: {
+        display: true,
+        position: "right",
+      },
+    },
+    cutout: "30%", // 도넛 차트의 가운데 구멍 크기 조절
+  };
+
+  return (
+    <div className="w-full h-full p-4 flex justify-center flex-col items-center">
+      <div className="h-56 overflow-hidden">
+        <Doughnut data={data} options={options} />
+      </div>
+      <div className="h-56 overflow-hidden">
+        <Doughnut data={data} options={options} />
+      </div>
+    </div>
+  );
+}
+
+export default StatChart;
--- src/components/StatChartCard.tsx
+++ src/components/StatChartCard.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import StatChart from "./StatChart";
 
 const StatChartCard = () => {
   return (
-    <div className="w-full max-w-md mx-auto bg-gray-100 shadow-md p-4"></div>
+    <div className="w-full max-w-md mx-auto bg-gray-100 shadow-md p-4">
+      <div className="bg-gray-300 p-2 flex justify-between items-center">
+        <div className="text-sm font-semibold">전적</div>
+        <div className="text-sm font-semibold text-blue-600">W 6</div>
+      </div>
+      <div className="flex items-center h-full">
+        <StatChart />
+      </div>
+    </div>
   );
 };
 
