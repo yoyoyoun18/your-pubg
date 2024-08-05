f3091073607391c24499c1b2216afd2a790106a9
kimyoungjo <yoyounn88@gmail.com>
2024-08-05 14:40:17 +0900

    feat: 암호화 id로 유저 랭크 정보를 받아오는 api 구현

---
+++ src/app/api/user/search/playertier/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const encryptedId = searchParams.get("encryptedId");
+
+  if (!encryptedId) {
+    return NextResponse.json({ error: "Missing encryptedId" }, { status: 400 });
+  }
+
+  try {
+    const { data } = await axios.get(
+      `https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/lXu1mvJOZEBnUs7wxWA91kgMVA93fC_mUluXt4DsPkE5JLeDHKEMZzwPxw`,
+      {
+        params: {
+          api_key: process.env.RIOT_API_KEY,
+        },
+      }
+    );
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error fetching Riot account:", error);
+    return NextResponse.json(
+      { error: "Error fetching Riot account" },
+      { status: 500 }
+    );
+  }
+}
