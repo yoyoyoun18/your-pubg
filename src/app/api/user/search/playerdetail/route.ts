3472c8c76a06d6f241f9b365f5e0986a96ddbc68
kimyoungjo <yoyounn88@gmail.com>
2024-08-05 11:46:59 +0900

    feat: puuid를 받아 유저의 레벨 등 상세정보를 받아오는 get 요청 api 코드 작성

---
+++ src/app/api/user/search/playerdetail/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const puuid = searchParams.get("puuid");
+
+  if (!puuid) {
+    return NextResponse.json(
+      { error: "Missing gameName or tagLine" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const { data } = await axios.get(
+      `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}`,
+      {
+        params: {
+          api_key: process.env.RIOT_API_KEY,
+        },
+      }
+    );
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error fetching Riot account:", error);
+    return NextResponse.json(
+      { error: "Error fetching Riot account" },
+      { status: 500 }
+    );
+  }
+}
