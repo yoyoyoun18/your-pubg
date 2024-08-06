06f744175de7c668a1a84abe8c8606d675cb29e2
kimyoungjo <yoyounn88@gmail.com>
2024-08-06 14:23:32 +0900

    init: 전적 추가 로드 버튼 구현

---
+++ src/components/AddMatch.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AddMatch() {
+  return (
+    <div className="w-full flex justify-center items-center cursor-pointer bg-gray-200 rounded-lg font-extrabold">
+      +
+    </div>
+  );
+}
+
+export default AddMatch;
