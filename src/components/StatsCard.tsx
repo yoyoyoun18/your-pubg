8f90f0897ee1fed5dc55f0c5e4882d50ef3c21ca
kimyoungjo <yoyounn88@gmail.com>
2024-08-07 19:20:18 +0900

    chore: 받아온 전역 상태 데이터들 배치

--- src/components/StatsCard.tsx
+++ src/components/StatsCard.tsx
@@ -1,6 +1,21 @@
+import useUserStore from "@/store/useUserStore";
 import React from "react";
 
 const StatsCard = () => {
+  const {
+    puuid,
+    gameName,
+    tagLine,
+    encryptedId,
+    accountId,
+    profileIconId,
+    revisionDate,
+    summonerLevel,
+    tier,
+    leaguePoints,
+    wins,
+    losses,
+  } = useUserStore((state) => state.targetUser);
   return (
     <div className="bg-white shadow-lg overflow-hidden w-full max-w-4xl h-[150px]">
       <div className="bg-[#5383E8] p-2">
@@ -11,14 +26,17 @@ const StatsCard = () => {
           <span className="rounded-full bg-gray-400 h-[90px] w-[90px]"></span>
         </div>
         <div className="col-span-6 lg:col-span-4 p-2 flex justify-center items-center flex-col">
-          <span className="font-bold text-sm lg:text-base mb-1">
-            Challenger
-          </span>
-          <span className="text-xs">2400 LP</span>
+          <span className="font-bold text-xs lg:text-md mb-1">{tier}</span>
+          <span className="text-xs">{leaguePoints} LP</span>
         </div>
         <div className="lg:col-span-4 p-2 justify-center items-center flex-col flex md:hidden lg:flex">
-          <span className="text-xs mb-1">254승 177패</span>
-          <span className="text-xs">승률 59%</span>
+          <span className="text-xs mb-1">
+            {wins}승 {losses}패
+          </span>
+          <span className="text-xs">
+            승률: {((Number(wins) / (Number(losses) + Number(wins))) * 100) | 0}
+            {/*승률 계산*/}%
+          </span>
         </div>
       </div>
     </div>
