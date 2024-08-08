4ef270d8a6963e865854578fcb367034b11ece0b
kimyoungjo <yoyounn88@gmail.com>
2024-08-08 19:02:48 +0900

    feat: axios all 을 사용하여 puuid를 이용해 요청할 수 있는 매치 id들을 가져옴

--- src/app/api/user/search/playerdetail/route.ts
+++ src/app/api/user/search/playerdetail/route.ts
@@ -6,27 +6,40 @@ export async function GET(request: NextRequest) {
   const puuid = searchParams.get("puuid");
 
   if (!puuid) {
-    return NextResponse.json(
-      { error: "Missing gameName or tagLine" },
-      { status: 400 }
-    );
+    return NextResponse.json({ error: "Missing puuid" }, { status: 400 });
   }
 
   try {
-    const { data } = await axios.get(
-      `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}`,
-      {
-        params: {
-          api_key: process.env.RIOT_API_KEY,
-        },
-      }
-    );
-    console.log("puuid 정상적으로 전달 받고 요청 드감");
-    return NextResponse.json(data);
+    const [summonerData, matchesData] = await axios.all([
+      axios.get(
+        `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}`,
+        {
+          params: {
+            api_key: process.env.RIOT_API_KEY,
+          },
+        }
+      ),
+      axios.get(
+        `https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids`,
+        {
+          params: {
+            start: 0,
+            count: 5,
+            api_key: process.env.RIOT_API_KEY,
+          },
+        }
+      ),
+    ]);
+
+    console.log("Successfully received data for both requests");
+    return NextResponse.json({
+      summoner: summonerData.data,
+      matches: matchesData.data,
+    });
   } catch (error) {
-    console.error("Error fetching Riot account:", error);
+    console.error("Error fetching Riot data:", error);
     return NextResponse.json(
-      { error: "Error fetching Riot account" },
+      { error: "Error fetching Riot data" },
       { status: 500 }
     );
   }
