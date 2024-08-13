0eeb9ab6389fdccefe6ebdfff0a6b236b3fa7cb7
kimyoungjo <yoyounn88@gmail.com>
2024-08-13 15:16:02 +0900

    init: 매치정보 api 백엔드로 이관

--- src/app/api/user/search/matchdetail/route.ts
+++ src/app/api/user/search/matchdetail/route.ts
@@ -1,34 +1,59 @@
-import { NextRequest, NextResponse } from "next/server";
+// File: app/api/user/search/match-details/route.ts
+
+import { NextResponse } from "next/server";
 import axios from "axios";
-import useUserStore from "@/store/useUserStore";
 
-export async function GET(request: NextRequest) {
-  const { matches } = useUserStore.getState().targetUser;
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const matchId = searchParams.get("matchId");
+  const targetPuuid = searchParams.get("targetPuuid");
 
-  if (!matches || matches.length === 0) {
-    return NextResponse.json({ error: "No matches found" }, { status: 400 });
+  if (!matchId || !targetPuuid) {
+    return NextResponse.json(
+      { error: "Missing matchId or targetPuuid" },
+      { status: 400 }
+    );
   }
 
   try {
-    const requests = matches.map((matchId) =>
-      axios.get(
-        `https://asia.api.riotgames.com/lol/match/v5/matches/KR_7210151713`,
-        {
-          params: {
-            api_key: process.env.RIOT_API_KEY,
-          },
-        }
-      )
+    const response = await axios.get(
+      `https://asia.api.riotgames.com/lol/match/v5/matches/${matchId}`,
+      {
+        params: {
+          api_key: process.env.RIOT_API_KEY,
+        },
+      }
     );
 
-    const responses = await axios.all(requests);
-    const matchesData = responses.map((response) => response.data);
+    const matchData = response.data;
+    const targetParticipant = matchData.info.participants.find(
+      (participant: { puuid: string }) => participant.puuid === targetPuuid
+    );
 
-    return NextResponse.json(matchesData);
+    if (targetParticipant) {
+      return NextResponse.json({
+        ...matchData,
+        info: {
+          ...matchData.info,
+          participants: [targetParticipant],
+        },
+      });
+    } else {
+      return NextResponse.json(
+        { error: "Target participant not found" },
+        { status: 404 }
+      );
+    }
   } catch (error) {
-    console.error("Error fetching Riot matches:", error);
+    console.error(`Error fetching match details for ${matchId}:`, error);
+    if (axios.isAxiosError(error)) {
+      return NextResponse.json(
+        { error: error.message },
+        { status: error.response?.status || 500 }
+      );
+    }
     return NextResponse.json(
-      { error: "Error fetching Riot matches" },
+      { error: "Internal Server Error" },
       { status: 500 }
     );
   }
