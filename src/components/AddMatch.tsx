d4c5f62346e496aa3a0c4d85a4926c9ad4a0c7bd
kimyoungjo <yoyounn88@gmail.com>
2024-08-14 14:39:14 +0900

    feat: AddMatch 컴포넌트에 matchCount를 늘릴 수 있는 increment를 onclick으로 접근할 수 있게 구현

--- src/components/AddMatch.tsx
+++ src/components/AddMatch.tsx
@@ -1,8 +1,14 @@
+import useMatchCounterStore from "@/store/useMatchCounterStore ";
 import React from "react";
 
 function AddMatch() {
+  const { matchCount, increment } = useMatchCounterStore();
+
   return (
-    <div className="w-full flex justify-center items-center cursor-pointer bg-gray-200 rounded-lg font-extrabold hover:bg-gray-100 shadow-md">
+    <div
+      className="w-full flex justify-center items-center cursor-pointer bg-gray-200 rounded-lg font-extrabold hover:bg-gray-100 shadow-md"
+      onClick={increment}
+    >
       +
     </div>
   );
