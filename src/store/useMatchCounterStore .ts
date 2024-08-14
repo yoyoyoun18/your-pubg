906fd48b70dd1705a9ac60857b8ade0d7a605a9d
kimyoungjo <yoyounn88@gmail.com>
2024-08-14 14:21:58 +0900

    chore: count를 좀 더 직관적인 변수명인 matchCount로 변경

--- src/store/useMatchCounterStore .ts
+++ src/store/useMatchCounterStore .ts
@@ -1,15 +1,15 @@
 import { create } from "zustand";
 
 interface MatchCounterState {
-  count: number;
+  matchCount: number;
   increment: () => void;
   reset: () => void;
 }
 
 const useMatchCounterStore = create<MatchCounterState>((set) => ({
-  count: 0,
-  increment: () => set((state) => ({ count: state.count + 1 })),
-  reset: () => set({ count: 0 }),
+  matchCount: 0,
+  increment: () => set((state) => ({ matchCount: state.matchCount + 1 })),
+  reset: () => set({ matchCount: 0 }),
 }));
 
 export default useMatchCounterStore;
